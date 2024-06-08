c268e68b53a093a471055f15d9bbe642ddc9900f
Lankiman <marvellousgboun@gmail.com>
2024-06-08 22:06:41 +0100

    Populating Models

---
+++ Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace e_learning.Models
+{
+    public class UserModel
+    {
+        [Required] public int Id { get; set; }
+
+        [Required] public string UserName { get; set; }
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        [Required] public string Email { get; set; }
+
+        [Required] public string Password { get; set; }
+
+        [Required] public string PhoneNumber { get; set; }
+    }
+}
