f3c472acff6b58769d6fc2a22916a7fadd52b354
Lankiman <marvellousgboun@gmail.com>
2024-06-08 20:38:07 +0100

    getting started

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_learning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
