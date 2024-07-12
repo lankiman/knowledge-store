e08e73f0f926741e5e041b8c20186c43e2570131
Lankiman <marvellousgboun@gmail.com>
2024-07-12 19:46:56 +0100

    adding functionalities

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,7 +8,57 @@ const menu = document.querySelector("#menu");
 const first = document.querySelector("#first");
 const second = document.querySelector("#second");
 
-burger.addEventListener('click', toogle);
+const passwordInput = document.querySelector(".password__input");
+
+const passwordVisible = document.querySelector(".password__input--visible");
+
+const passwordNotVisible = document.querySelector(".password__input--hidden");
+
+const confirmPasswordInput = document.querySelector(".confirm__password__input");
+
+const confirmPasswordVisible = document.querySelector(".confirm__password__input--visible");
+
+const confirmPasswordNotVisible = document.querySelector(".confirm__password__input--hidden");
+
+
+
+if (passwordInput) {
+    if (passwordNotVisible) {
+        passwordNotVisible.addEventListener("click", () => {
+            passwordInput.setAttribute("type", "text");
+            passwordNotVisible.classList.add("hidden");
+            passwordVisible.classList.remove("hidden");
+        })
+    }
+    if (passwordVisible) {
+        passwordVisible.addEventListener("click", () => {
+            passwordInput.setAttribute("type", "password");
+            passwordVisible.classList.add("hidden");
+            passwordNotVisible.classList.remove("hidden");
+        })
+    }
+}
+
+if (confirmPasswordInput) {
+    if (passwordNotVisible) {
+        confirmPasswordNotVisible.addEventListener("click", () => {
+            confirmPasswordInput.setAttribute("type", "text");
+            confirmPasswordNotVisible.classList.add("hidden");
+            confirmPasswordVisible.classList.remove("hidden");
+        })
+    }
+    if (passwordVisible) {
+        confirmPasswordVisible.addEventListener("click", () => {
+            confirmPasswordInput.setAttribute("type", "password");
+            confirmPasswordVisible.classList.add("hidden");
+            confirmPasswordNotVisible.classList.remove("hidden");
+        })
+    }
+}
+
+if (burger) {
+    burger.addEventListener('click', toogle);
+}
 
 function toogle() {
     if (menu.classList.contains('hidden')) {
