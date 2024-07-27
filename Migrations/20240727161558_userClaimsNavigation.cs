3fb13fb20c90aedc230e2b3031d4474dae9b2e7e
Lankiman <marvellousgboun@gmail.com>
2024-07-27 17:20:54 +0100

    migrated new navigation property [skip ci]

---
+++ Migrations/20240727161558_userClaimsNavigation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace e_learning.Migrations
+{
+    /// <inheritdoc />
+    public partial class userClaimsNavigation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IdentityUserClaim<int>",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserModelId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityUserClaim<int>", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IdentityUserClaim<int>_AspNetUsers_UserModelId",
+                        column: x => x.UserModelId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IdentityUserClaim<int>_UserModelId",
+                table: "IdentityUserClaim<int>",
+                column: "UserModelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdentityUserClaim<int>");
+        }
+    }
+}
