d96a742837e96cc349a8d69d3d3c235459e71778
Lankiman <marvellousgboun@gmail.com>
2024-10-07 14:22:04 +0100

    added rederiction of learners to the appropriate controller action

--- Controllers/LearnerController.cs
+++ Controllers/LearnerController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace e_learning.Controllers
 {
     public class LearnerController : Controller
     {
+        [Authorize(Roles = "Learner")]
         public IActionResult Dashboard()
         {
             return View();
