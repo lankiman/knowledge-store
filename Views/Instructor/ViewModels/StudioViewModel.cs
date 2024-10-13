90ebc9838113be503da70636b0db42ef340c3c7c
Lankiman <marvellousgboun@gmail.com>
2024-10-13 23:36:10 +0100

    create few new partieal views and view models

---
+++ Views/Instructor/ViewModels/StudioViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace e_learning.Views.Instructor.ViewModels
+{
+    public class StudioViewModel(string activeView, UploadVideoViewModel lessonModel)
+    {
+        public string ActiveView { get; set; } = activeView;
+        public UploadVideoViewModel LessonModel { get; set; } = lessonModel;
+        public static StudioViewModel ForLessonUpload(UploadVideoViewModel model) => new("lesson", model);
+    }
+
+
+}
